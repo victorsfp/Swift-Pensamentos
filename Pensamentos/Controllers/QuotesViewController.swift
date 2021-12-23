5d3db30978c4df35599204821fc327c7a6638406
Victor Feitosa <victor.s.feitosa2@hotmail.com>
2021-12-23 18:36:56 -0300

    Finally project

--- Pensamentos/Controllers/QuotesViewController.swift
+++ Pensamentos/Controllers/QuotesViewController.swift
@@ -9,27 +9,40 @@ class QuotesViewController: UIViewController {
     @IBOutlet weak var lbAuthor: UILabel!
     @IBOutlet weak var lcTop: NSLayoutConstraint!
     
+    let config = Configuration.shared
     let quotesManager = QuotesManager()
     var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.s
+        NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "Refresh"), object: nil, queue: nil) { (notification) in
+            self.formatView()
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        prepateQuote()
+        formatView()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         prepateQuote()
     }
     
+    func formatView(){
+        view.backgroundColor = config.colorScheme == 0 ? .white : UIColor(red: 156.0/255.0, green: 68.0/255.0, blue: 15.0/255.0, alpha: 1.0)
+        lbQuotes.textColor = config.colorScheme == 0 ? .black : .white
+        lbAuthor.textColor = config.colorScheme == 0 ? UIColor(red: 192.0/255.0, green: 96.0/255.0, blue: 49.0/255.0, alpha: 1.0) : .yellow
+        prepateQuote()
+    }
+    
     func prepateQuote(){
         timer?.invalidate()
-        timer = Timer.scheduledTimer(withTimeInterval: 8.0, repeats: true) { (timer) in
-            self.showRandomQuote()
+        if config.autoRefresh {
+            timer = Timer.scheduledTimer(withTimeInterval: config.timeInterval, repeats: true) { (timer) in
+                self.showRandomQuote()
+            }
         }
         showRandomQuote()
     }
--- Pensamentos/Controllers/SettingsViewController.swift
+++ Pensamentos/Controllers/SettingsViewController.swift
@@ -7,19 +7,44 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var scColorScheme: UISegmentedControl!
     @IBOutlet weak var lbTimerInterval: UILabel!
     
+    let config = Configuration.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "Refresh"), object: nil, queue: nil) { (notification) in
+            self.formatView()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        formatView()
+    }
+    
+    func formatView(){
+        swAutoRefresh.setOn(config.autoRefresh, animated: false)
+        slTimerInterval.setValue(Float(config.timeInterval), animated: false)
+        scColorScheme.selectedSegmentIndex = config.colorScheme
+        changeTimeIntervalLabel(with: config.timeInterval)
+    }
+    
+    func changeTimeIntervalLabel(with value: Double){
+        lbTimerInterval.text = "Mudar após \(Int(value)) segundos"
     }
     
     @IBAction func changeAutoRefresh(_ sender: UISwitch) {
+        config.autoRefresh = sender.isOn
     }
    
     @IBAction func changeTimerInterval(_ sender: UISlider) {
+        let value = Double(round(sender.value))
+        changeTimeIntervalLabel(with: value)
+        config.timeInterval = value
     }
     
     @IBAction func changeColorScheme(_ sender: UISegmentedControl) {
+        config.colorScheme = sender.selectedSegmentIndex
     }
     
 }
